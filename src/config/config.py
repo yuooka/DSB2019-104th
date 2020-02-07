db63b0e47e2e5bb1c7ca81d67042cd48ecfdd822
yuooka <60316115+yuooka@users.noreply.github.com>
2020-02-08 08:54:35 +0900

    Add files via upload

---
+++ src/config/config.py
@@ -0,0 +1,15 @@
+SEED = 100
+FOLDS = 5
+N_FOLD = 0
+
+SEED_AVG_TIMES = 5
+
+VALID_TH = [1.1, 1.6, 2.2]
+
+TRAIN_DIR = '/kaggle/input/data-science-bowl-2019/train.csv'
+TRAIN_LABELS_DIR = '/kaggle/input/data-science-bowl-2019/train_labels.csv'
+SPECS_DIR = '/kaggle/input/data-science-bowl-2019/specs.csv'
+TEST_DIR = '/kaggle/input/data-science-bowl-2019/test.csv'
+SAMPLE_SUBMISSION_DIR = '/kaggle/input/data-science-bowl-2019/sample_submission.csv'
+
+OUT_DIR = './output'
